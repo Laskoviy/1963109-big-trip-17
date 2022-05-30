aa5196abbafe7c2934eb2dcf8deedd9db94bf229
Жиделев Максим Александрович <mzhidelev@yandex.ru>
2022-05-30 22:24:24 +0300

    final-fix

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,9 +2,9 @@ import { FilterType } from '../const';
 import { isPointAhead, isPointExpired } from './event';
 
 const filter = {
-  [FilterType.EVERYTHING]: (points) => points.filter((point) => !point.isArchive),
-  [FilterType.FUTURE]: (points) => points.filter((point) => isPointAhead(point.dueDate) && !point.isArchive),
-  [FilterType.PAST]: (points) => points.filter((point) => isPointExpired(point.dueDate) && !point.isArchive),
+  [FilterType.EVERYTHING]: (points) => points,
+  [FilterType.FUTURE]: (points) => points.filter((point) => isPointAhead(point.dateFrom)), //дата начала события больше или равна текущей дате;
+  [FilterType.PAST]: (points) => points.filter((point) => isPointExpired(point.dateTo)),//дата окончания маршрута меньше, чем текущая
 };
 
 export { filter };
--- src/view/filterView.js
+++ src/view/filterView.js
@@ -1,7 +1,7 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 const createFilterItemTemplate = (filter, isChecked) => {
-  const { name } = filter;
+  const { name, count } = filter;
   //в случае отсутствия данных по фильтру, он блокируется от выбора нужно придумать как реализовать
   return (
     `<div class="trip-filters__filter">
@@ -11,23 +11,7 @@ const createFilterItemTemplate = (filter, isChecked) => {
       type="radio"
       name="trip-filter"
       value="everything" ${isChecked ? 'checked' : ''}
-      />
-      <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
-     </div>`
-  );
-};
-
-const createFilterItemTemplateDisabled = (filter, isChecked) => {
-  const { name } = filter;
-  //в случае отсутствия данных по фильтру, он блокируется от выбора нужно придумать как реализовать
-  return (
-    `<div class="trip-filters__filter">
-      <input
-      id="filter-${name}" disabled
-      class="trip-filters__filter-input  visually-hidden"
-      type="radio"
-      name="trip-filter"
-      value="everything" ${isChecked ? 'checked' : ''}
+      ${count < 1 ? 'disabled' : ''}
       />
       <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
      </div>`
@@ -45,17 +29,6 @@ const createFilterTemplate = (filterItems) => {
     </form>`;
 };
 
-const createFilterTemplateDisabled = (filterItems) => {
-  const filterItemsTemplate = filterItems
-    .map((filter, index) => createFilterItemTemplateDisabled(filter, index === 0))
-    .join('');
-
-  return `<form class="trip-filters" action="#" method="get">
-    ${filterItemsTemplate}
-    <button class="visually-hidden" type="submit">Accept filter</button>
-    </form>`;
-};
-
 export default class FilterView extends AbstractView {
   #filters = null;
 
@@ -65,10 +38,6 @@ export default class FilterView extends AbstractView {
   }
 
   get template() {
-    if (this.#filters.count<1) {
-      return createFilterTemplateDisabled(this.#filters);
-    } else {
-      return createFilterTemplate(this.#filters);
-    }
+    return createFilterTemplate(this.#filters);
   }
 }
